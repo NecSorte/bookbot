9cb2704ee5ad79b1af29d23f3d4467b4b8cc96e3
NecSorte <69991710+NecSorte@users.noreply.github.com>
2025-12-20 14:28:11 -0800

    ready

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import sys
 
 
 def main():
-    path = sys.argv[1]
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
-    text = get_book_text(path)
-    print("----------- Word Count ----------")
-    count_words(text)
-    print("--------- Character Count -------")
-    lowers = lower_character(text)
-    sorted_chars = sort_letters(lowers)
-    print_dictionary_list(sorted_chars)
+    if len(sys.argv) < 2: 
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        text = get_book_text(path)
+        print("----------- Word Count ----------")
+        count_words(text)
+        print("--------- Character Count -------")
+        lowers = lower_character(text)
+        sorted_chars = sort_letters(lowers)
+        print_dictionary_list(sorted_chars)
 
 
 def get_book_text(path):
