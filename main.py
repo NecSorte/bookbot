4535854f4e2c7b616f2886b131d340a190b1c943
NecSorte <69991710+NecSorte@users.noreply.github.com>
2025-12-19 20:07:30 -0800

    yay

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def main():
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    count_words(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return print(f"Found {len(words)} total words")
+
+main()
