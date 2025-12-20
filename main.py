9164d9dfaece375e0d0c1c24c3e923f06bb45ee2
NecSorte <69991710+NecSorte@users.noreply.github.com>
2025-12-19 21:04:50 -0800

    yay

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from stats import count_words
+from stats import count_words, lower_character
 
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     count_words(text)
+    lowers = lower_character(text)
+    print(lowers)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def count_words(text):
     words = text.split()
     return print(f"Found {len(words)} total words")
+
+def lower_character(text):
+    dict = {}
+
+    for letter in text:
+        letter = str.lower(letter)
+        if letter in dict:
+            # Do add the value of the key + 1
+            dict[letter] = dict[letter] + 1
+        else:
+            # add letter to dict
+            dict[letter] = 1
+    return dict
+
+    # values = 
+    # for letter in text:
+    #     #add to dict
+    #     lower = str.lower(text)
+    #     return lower
+    #     #integer of that  
